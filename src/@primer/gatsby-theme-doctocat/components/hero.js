bbbe0d550cc6f2f04d5ad35abe82bea3e11c9393
pjmelin201 <bosspijal180@gmail.com>
2025-05-11 16:38:33 +0800

    Initialize homepage-updates

---
+++ src/@primer/gatsby-theme-doctocat/components/hero.js
@@ -0,0 +1,19 @@
+import {Box, Heading, ThemeProvider} from '@primer/react'
+import React from 'react'
+import {Container} from '@primer/gatsby-theme-doctocat'
+import heroIllustration from '../../../hero-illustration.svg'
+
+export default function Hero() {
+  return (
+    <ThemeProvider colorMode="night" nightScheme="dark_dimmed">
+      <Box bg="canvas.default" py={6}>
+        <Container>
+          <Heading sx={{color: 'accent.fg', fontSize: 7, lineHeight: 'condensed', pb: 3, m: 0}}>
+            Primer Design System
+          </Heading>
+          <img src={heroIllustration} alt="Primer Design System" width="100%" />
+        </Container>
+      </Box>
+    </ThemeProvider>
+  )
+}
