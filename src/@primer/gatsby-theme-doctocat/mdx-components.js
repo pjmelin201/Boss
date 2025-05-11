dc46d044e0c3038e0107832bd8de2d15072be5cf
pjmelin201 <bosspijal180@gmail.com>
2025-05-11 16:38:21 +0800

    Initialize link-docs

---
+++ src/@primer/gatsby-theme-doctocat/mdx-components.js
@@ -0,0 +1,5 @@
+import CustomVideoPlayer from '../../CustomVideoPlayer'
+import ImageBox from './components/ImageBox'
+import StorybookEmbed from '../../components/storybook-embed'
+
+export default {CustomVideoPlayer, ImageBox, StorybookEmbed}
