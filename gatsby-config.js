c8e7cd42e5df9012ab862b2c1b0910b566c88794
pjmelin201 <bosspijal180@gmail.com>
2025-05-11 16:38:29 +0800

    Initialize howdyray-patch-1

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  siteMetadata: {
+    title: 'Primer',
+    // Remove short name from site header
+    shortName: '',
+    description: 'Principles, standards, and usage guidelines for designing GitHub interfaces.',
+  },
+  pathPrefix: '/design',
+  plugins: [
+    {
+      resolve: '@primer/gatsby-theme-doctocat',
+      options: {
+        defaultBranch: 'main',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          react: path.resolve(__dirname, 'node_modules', 'react'),
+          '~': path.resolve(__dirname),
+        },
+      },
+    },
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-svgr',
+      options: {
+        svgo: false,
+      },
+    },
+  ],
+}
