389a4923a8424fd1a9d6a756cf3eaee202bbcbd3
pjmelin201 <bosspijal180@gmail.com>
2025-05-11 16:38:50 +0800

    Initialize dipree/custom-head

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Contributing to Primer Interface Guidelines
+
+You can read about different ways to contribute to the Primer Design System in [primer.style/contribute](https://primer.style/contribute/how-to-contribute).
+
+If you are designing new components, please read the [design contribution guidelines](https://primer.style/contribute/design).
+
+If you are writing documentation, please read the [documentation guidelines](https://primer.style/contribute/documentation). |
---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  siteMetadata: {
+    title: 'Primer',
+    header: {
+      title: 'Primer Design System'
+    },
+    // Remove short name from site header
+    shortName: '',
+    description: 'Principles, standards, and usage guidelines for designing GitHub interfaces.',
+  },
+  plugins: [
+    {
+      resolve: '@primer/gatsby-theme-doctocat',
+      options: {
+        defaultBranch: 'main',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-alias-imports',
+      options: {
+        alias: {
+          react: path.resolve(__dirname, 'node_modules', 'react'),
+          '~': path.resolve(__dirname),
+        },
+      },
+    },
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-svgr',
+      options: {
+        svgo: false,
+      },
+    },
+    'gatsby-plugin-react-helmet'
+  ],
+}
